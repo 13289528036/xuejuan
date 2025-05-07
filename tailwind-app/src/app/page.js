5c2e8e2f2bf14737817ab5c028b2554510c411ea
Your Name <you@example.com>
2025-05-07 22:11:21 +0800

    feat: 初始化Next.js项目并集成Tailwind CSS
    初始化一个基于Next.js的项目，并集成Tailwind CSS用于样式管理。包括配置文件、依赖项、页面组件和全局样式的设置。此项目为后续开发提供了一个基础框架。

---
+++ tailwind-app/src/app/page.js
@@ -0,0 +1,10 @@
+import Navbar from "../components/Navbar";
+export default function Hello() {
+  return (
+    <div className="min-h-screen">
+      <Navbar />
+      <h1 className="text-red-500 bg-sky-50 text-6xl text-center py-4">欢迎来到雪娟页面</h1>
+      
+    </div>
+  )
+}
